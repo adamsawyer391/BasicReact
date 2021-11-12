4f59e0dae925b2069b4655291eec8267540ffc9c
Adam Sawyer <39273302+adamsawyer391@users.noreply.github.com>
2021-11-11 19:29:05 -0500

    second commit - hooks

--- src/App.js
+++ src/App.js
@@ -1,12 +1,220 @@
 
-import './App.css';
+// // import './App.css';
+// import React, { useState } from 'react';
 
-function App() {
+// const App = (props) => {
+//   const [people, setPeople] = useState([
+//     {
+//       firstName: 'John',
+//       lastName: 'Smith'
+//     },
+//     {
+//       firstName: 'Jane',
+//       lastName: 'Doe'
+//     }
+//   ]);
+//   const [person, setPerson] = useState({ firstName: '', lastName: '' });
+//   const onChange = (event) => {
+//     setPerson({ ...person, [event.target.value] : event.target.value});
+//   }
+//   const onSubmit = (event) => {
+//     event.preventDefault();
+//     if(person.firstName.trim() === '' || person.lastName.trim() === '') return;
+
+//     const newPerson = {
+//       firstName: person.firstName.trim(),
+//       lastName: person.lastName.trim(),
+//     };
+//     setPeople([...people, newPerson]);
+
+//   }
+  
+//   return (
+//     <div className="container">
+//       <div className="row">
+//         <div className="col">
+//           <h2>Add a Person</h2>
+//           <hr />
+//           <form onSubmit={onSubmit}>
+//             <div className="form-group">
+//               <input 
+//                 type="text"
+//                 name="firstName"
+//                 placeholder="First Name"
+//                 value={person.firstName}
+//                 onChange={onChange}
+//                 className="form-control"
+//               />
+//             </div>
+//             <br />
+//             <div className="form-group">
+//               <input 
+//                 type="text"
+//                 name="lastName"
+//                 placeholder="Last Name"
+//                 value={person.lastName}
+//                 onChange={onChange}
+//                 className="form-control"
+//               />
+//             </div>
+//             <br />
+//             <button className="btn btn-success" type="submit">Add Person</button>
+//           </form>
+//         </div>
+//         <div className="col">
+//           <h2>People:</h2>
+//           <hr />
+//           {
+//             people.map(p => (
+//               <div key={Math.random() * 1000000000 }>
+//                 <p> {p.firstName} {p.lastName} </p>
+//               </div>
+//             ))
+//           }
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/**
+ * This is the one that works. Now commenting it out for part II: useEffect. This was for episode 1: useState()
+ */
+
+// import React, { useState } from 'react';
+
+// const App = () => {
+//   const [people, setPeople] = useState([
+//     {
+//       firstName: 'John',
+//       lastName: 'Doe'
+//     },
+//     {
+//       firstName: 'Jane',
+//       lastName: 'Doe'
+//     }
+//   ]);
+//   const [person, setPerson] = useState({ firstName: '', lastName: '' });
+//   const onChange = (event) => {
+//     setPerson({ ...person, [event.target.name]: event.target.value });
+//   };
+//   const onSubmit = (event) => {
+//     event.preventDefault();
+//     if (person.firstName.trim() === '' || person.lastName.trim() === '') return;
+
+//     const newPerson = {
+//       firstName: person.firstName.trim(),
+//       lastName: person.lastName.trim()
+//     };
+
+//     setPeople([...people, newPerson]);
+//     setPerson({ firstName: '', lastName: '' });
+//   };
+//   return (
+//     <div className="container">
+//       <div className="row">
+//         <div className="col">
+//           <h2>Add a person: </h2>
+//           <hr />
+//           <form onSubmit={onSubmit}>
+//             <div className="form-group">
+//               <input
+//                 type="text"
+//                 className="form-control"
+//                 name="firstName"
+//                 placeholder="First Name.."
+//                 value={person.firstName}
+//                 onChange={onChange}
+//               />
+//             </div>
+//             <div className="form-group">
+//               <input
+//                 type="text"
+//                 className="form-control"
+//                 name="lastName"
+//                 placeholder="Last Name.."
+//                 value={person.lastName}
+//                 onChange={onChange}
+//               />
+//             </div>
+//             <button className="btn btn-success" type="submit">
+//               Add person
+//             </button>
+//           </form>
+//         </div>
+//         <div className="col">
+//           <h2>People: </h2>
+//           <hr />
+//           {people.map((p) => (
+//             <div key={Math.random() * 1000000000}>
+//               <p>
+//                 {p.firstName} {p.lastName}
+//               </p>
+//             </div>
+//           ))}
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default App;
+
+
+
+
+
+
+
+
+
+
+
+import React, { useState } from 'react';
+
+import Form from './components/Form';
+import People from './components/People';
+import NewestPerson from './components/NewestPerson';
+
+const App = () => {
+  const [people, setPeople] = useState([
+    {
+      firstName: 'John',
+      lastName: 'Doe'
+    },
+    {
+      firstName: 'Jane',
+      lastName: 'Doe'
+    }
+  ]);
+  const addPerson = (person) => {
+    setPeople([...people, person]);
+  };
+
   return (
-    <div className="App">
-      <h1>Hello World</h1>
+    <div className="container mt-4">
+      <div className="row">
+        <Form addPerson={addPerson} />
+        <People people={people} />
+        <NewestPerson newestPerson={people[people.length - 1]} />
+      </div>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/books.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+
+const books = [
+  {
+    url: "https://cdn.mos.cms.futurecdn.net/ka5acHNs3trfycsfvB5aeY.jpg",
+    title: "To the Stars",
+    author: "Ted Mosby"
+  },
+  {
+    url: "https://upload.wikimedia.org/wikipedia/commons/6/6b/Entering_a_Lunar_Outpost.jpg",
+    title: "Living On the Moon!",
+    author: "Marshall Erickson"
+  }
+]
+
+function BookList(){
+  return (
+    <div>
+      {
+        books.map((item, index) => {
+          return (
+            <section className="booklist">
+              <Book url={item.url} title={item.title} author ={item.author} />
+            </section>
+          )
+        })
+      }
+    </div>
+  )
+}
+
+const Book = ({url, title, author}) => {
+  // const { url, title, author } = props;
+  return (
+    <article className="book">
+      <img 
+        width="300px"
+        height="300px"
+        src={url}
+        alt="" 
+        style={{borderRadius: '10px'}}
+      />
+      <h2>{title}</h2>
+      <h4>{author.toUpperCase()}</h4>
+    </article>
+  )
+}
